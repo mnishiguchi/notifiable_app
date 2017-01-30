65037493a0e55025f0d986d8a721865429ffa2ce
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2017-01-30 11:43:55 -0500

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  resources :forum_threads do
+    resources :forum_posts
+  end
+
+  resources :notifications do
+    collection do
+      post :mark_as_read
+    end
+  end
+
+  root to: "forum_threads#index"
